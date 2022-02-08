406ca34dad6d5035f503d045346535774c4baf8e
White Pegasus <whitepegasus1603@gmail.com>
2022-02-08 13:57:09 +0530

    08-02-2022 2nd

---
+++ Strings/ReturnAllCodes.java
@@ -0,0 +1,62 @@
+/*
+Return all codes - String
+Send Feedback
+Assume that the value of a = 1, b = 2, c = 3, ... , z = 26. You are given a numeric string S. Write a program to return the list of all possible codes that can be generated from the given string.
+Note : The order of codes are not important. And input string does not contain 0s.
+Input format :
+A numeric string
+Constraints :
+1 <= Length of String S <= 10
+Sample Input:
+1123
+Sample Output:
+aabc
+kbc
+alc
+aaw
+kw
+*/
+
+public class solution 
+{ 
+    public static char getChar(int n)
+    { 
+        return (char)(96 + n); 
+        
+    } 
+    
+    public static String[] getCode(String input) 
+    { 
+        if(input.length() == 0) { 
+            String[] output = {""}; 
+            return output; 
+            
+        } 
+        int firstDigit = (input.charAt(0) - '0'); 
+        String[] smallOutput1 = getCode(input.substring(1)); 
+        String[] smallOutput2 = new String[0]; 
+        int firstTwoDigits = 0; 
+        if(input.length() >= 2) { firstTwoDigits = (input.charAt(0) - '0') * 10 + (input.charAt(1) - '0'); 
+        if(firstTwoDigits > 10 && firstTwoDigits<=26){ 
+            smallOutput2 = getCode(input.substring(2)); 
+            
+        } 
+            
+        } 
+        String output[] = new String[smallOutput1.length + smallOutput2.length]; 
+        int k = 0; for(String s : smallOutput1){ char firstDigitChar = getChar(firstDigit); // (char)('a'-1+lastDigit) 
+        output[k] = firstDigitChar + s; 
+        k++; 
+            
+        } 
+        for( String s : smallOutput2){ 
+            char firstTwoDigitChar = getChar(firstTwoDigits); 
+            output[k] = firstTwoDigitChar + s; 
+            k++; 
+            
+        } 
+        return output; 
+        
+    } 
+    
+}
